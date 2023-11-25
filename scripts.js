025b095dadc8d59154fc5d8cb927b7207c5da25a
Jonathon Leathers <jtleathers@gmail.com>
2023-11-25 15:07:57 -0800

    cleaned up the files: simplified the JS, added some markup and notes to HTML, renamed CSS file

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function replaceText() {
+function convertFontURL() {
 
     function filterAndExtractMinMax(arr, targetValue) {
         // Filter the arrays where the first value matches the target value
@@ -28,8 +28,8 @@ function replaceText() {
     // Use a regular expression to find all occurrences of "family="
     var matches = url.match(/family=[^&]*/g);
 
-    // Check if matches is not null (i.e., "family=" was found) and if it has more than 2 font families
-    if ( matches !== null && matches.length >= 2 ) {
+    // Check if matches is not null (i.e., "family=" was found)
+    if ( matches !== null ) {
 
         // Extract everything from "family=" until the next "&"
         var fontFamiliesArray = matches.map(function(match) {
@@ -47,24 +47,24 @@ function replaceText() {
             var fontFamilyWeights = fontFamily[1];
 
             // Build an array and convert strings to numbers
-            var textToArray2 = fontFamilyWeights.split(';').map(function(item) {
+            var textToArray = fontFamilyWeights.split(';').map(function(item) {
                 return item.split(',').map(function(value) {
                     return parseInt(value, 10);
                 });
             });
 
             // Replace the current weights array with this new array
-            fontFamily[1] = textToArray2;
+            fontFamily[1] = textToArray;
 
             // Check if the font family has both normal and italic weights
             if ( fontFamily[1][0].length > 1 ) {
                 // Normal AND Italic 
-                var resultStringForZero2 = filterAndExtractMinMax(fontFamily[1], 0);
-                var resultStringForOne2 = filterAndExtractMinMax(fontFamily[1], 1);
+                var resultStringForZero = filterAndExtractMinMax(fontFamily[1], 0);
+                var resultStringForOne = filterAndExtractMinMax(fontFamily[1], 1);
                 
-                var combined3 = 'family=' + fontFamily[0] + '0,' + resultStringForZero2 + ';' + '1,' + resultStringForOne2 + '&';
+                var combinedNormalItalic = 'family=' + fontFamily[0] + '0,' + resultStringForZero + ';' + '1,' + resultStringForOne + '&';
 
-                newURL += combined3;
+                newURL += combinedNormalItalic;
 
             } else {
                 // Normal OR Italic 
@@ -77,13 +77,13 @@ function replaceText() {
                 var maxValue = Math.max.apply(null, flattenedArray2);
 
                 // Create a new array with only the minimum and maximum values
-                var resultArray2 = [minValue, maxValue];
+                var resultArray = [minValue, maxValue];
 
-                var resultString2 = resultArray2.join('..');
+                var resultString = resultArray.join('..');
                 
-                var combined2 = 'family=' + fontFamily[0] + resultString2 + '&';
+                var combinedNormal = 'family=' + fontFamily[0] + resultString + '&';
 
-                newURL += combined2;
+                newURL += combinedNormal;
 
             }
 
@@ -91,62 +91,14 @@ function replaceText() {
 
         newURL = newURL + 'display=swap';
         
-        var updatedURL2 = url.replace(/family=.*?&display=swap/, newURL);                
+        var updatedURL = url.replace(/family=.*?&display=swap/, newURL);                
         
-        // Display the resulting url
-        document.getElementById("result").innerHTML = updatedURL2;
-
-        // Display the resulting code
-        document.getElementById("result-code").innerHTML = '&lt;link rel="preconnect" href="https://fonts.googleapis.com"&gt;<br>&lt;link rel="preconnect" href="https://fonts.gstatic.com" crossorigin&gt;<br>&lt;link href="' + updatedURL2 + '" rel="stylesheet"&gt;';
-
-    } else if ( matches !== null && matches.length == 1 ) {
-        // Check if matches is not null (i.e., "family=" was found) and if it has only 1 font family
-
-        // Find everything between wght@ and &display
-        var extractedText = url.match(/wght@([^&]*)\&display/g);
-        
-        // Remove wght@ and &display
-        var modifiedText = extractedText[0].replace(/wght@/, '').replace(/&display/, '');
-
-        // Build an array and convert strings to numbers
-        var textToArray = modifiedText.split(';').map(function(item) {
-            return item.split(',').map(function(value) {
-                return parseInt(value, 10);
-            });
-        });
-
-        if ( textToArray[0].length > 1 ) {
-            // This is if both normal and italic are passed in url
-            var resultStringForZero = filterAndExtractMinMax(textToArray, 0);
-            var resultStringForOne = filterAndExtractMinMax(textToArray, 1);
-            
-            var combined = 'wght@0,' + resultStringForZero + ';1,' + resultStringForOne + '&display';
-        } else {
-            // This is if only normal or italic are passed in url
-            var flattenedArray = textToArray.map(function(subarray) {
-                return subarray[0];
-            });
-
-            // Find the minimum and maximum values
-            var minValue = Math.min.apply(null, flattenedArray);
-            var maxValue = Math.max.apply(null, flattenedArray);
-
-            // Create a new array with only the minimum and maximum values
-            var resultArray = [minValue, maxValue];
-
-            var resultString = resultArray.join('..')
-            
-            var combined = 'wght@' + resultString + '&display';
-        }
-
-        // Search for the specified string in testURL and replace it with combined
-        updatedURL = url.replace(/wght@.*?&display/, combined);
-
         // Display the resulting url
         document.getElementById("result").innerHTML = updatedURL;
 
         // Display the resulting code
-        document.getElementById("result-code").innerHTML = '&lt;link rel="preconnect" href="https://fonts.googleapis.com"&gt;<br>&lt;link rel="preconnect" href="https://fonts.gstatic.com" crossorigin&gt;<br>&lt;link href="' + updatedURL + '" rel="stylesheet"&gt;';
+        document.getElementById("result-code").innerHTML = '&lt;link rel="preconnect" href="https://fonts.googleapis.com"&gt;<br>&lt;link rel="preconnect" href="https://fonts.gstatic.com" crossorigin&gt;<br>&lt;link href="' + updatedURL2 + '" rel="stylesheet"&gt;';
+
     } else {
         // Display the result
         document.getElementById("result").innerHTML = "This is not a Google Fonts URL.";
